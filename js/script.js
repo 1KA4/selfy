077da3853f53db0048b942e235add53e5aeb5430
ivanstud <kachur140701@gmail.com>
2021-06-04 22:33:33 +0200

    Without Slider:(

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+    
+    jQuery(document).ready(function ($) {
+                        /* Mobile Nav */
+            ///////////////////////////////////////////
+        // Burger Button
+        $(".burger__container").on("click", function(){
+            let navToggle = $("#navToggle");
+            let nav = $("#nav");
+
+            $(this).toggleClass("change");
+
+            nav.toggleClass("display");
+        });
+
+        // Slider
+        
+      
+      });    
+      
